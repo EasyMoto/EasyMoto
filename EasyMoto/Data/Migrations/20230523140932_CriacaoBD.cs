73be269bf72a7fc15a60a2776c58189ba3d1e567
Ana Graca <Ana.Graca@kyndryl.com>
2023-05-23 15:10:35 +0100

    Criação da ligações das tabelas e Migração

---
+++ EasyMoto/Data/Migrations/20230523140932_CriacaoBD.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyMoto.Data.Migrations
+{
+    public partial class CriacaoBD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
